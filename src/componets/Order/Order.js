7a0f5b40cf2fb59fc97764fc30cd046ce2b8d782
unknown <you@example.com>
2021-11-10 22:42:55 +0600

    done

--- src/componets/Order/Order.js
+++ src/componets/Order/Order.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import { useForm } from "react-hook-form";
 
 const Order = () => {
     const [orders, setOrder] = useState([])
     const { productID } = useParams();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+    const { user } = useAuth()
     useEffect(() => {
         const url = `http://localhost:5000/products`;
 
@@ -14,7 +21,7 @@ const Order = () => {
 
     }, []);
     // Filtatring Producuts
-
+    console.log(user)
 
     const order = orders?.filter(items => items._id == productID);
 
@@ -25,7 +32,61 @@ const Order = () => {
 
     return (
         <div>
-            <h3>{order[0]?.name}</h3>
+            <div className="container">
+
+
+                <div class="row">
+                    <div className="col-lg-7 col-sm-12">
+                        <div className="details my-5  justify-content-center">
+
+                            <h2 className="text-secondary">{order[0]?.name}</h2>
+                            <h4 className="text-secondary">{order[0]?.subname}</h4>
+                            <hr />
+                            <h3><img className="img-fluid img-rounded" src={order[0]?.img} /></h3>
+                            <h3 className="text-muted">{order[0]?.type}</h3>
+                            <h4 className="text-muted">Price : {order[0]?.price}$</h4>
+                            <p>{order[0]?.discription}</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-5 col-sm-12 my-5" >
+                        <div className="my-5">
+                            <h2>Order Now</h2>
+                            <div className="mx-auto my-5">
+                                <div className="my-4">
+                                    <h3>Customer:<span className="text-muted"> {user.email}</span></h3>
+                                    <h3>Name:<span className="text-muted"> {user.displayName}</span></h3>
+                                </div>
+                                <div>
+                                    <form onSubmit={handleSubmit(onSubmit)}>
+                                        <input style={{ width: "50%" }} type="email" defaultValue={user.email} {...register("email")} />
+                                        <br />
+                                        <br />
+                                        <input placeholder="Enter Your Name " style={{ width: "50%" }} type="name" {...register("name", { required: true })} />
+                                        <br />
+                                        {errors.exampleRequired && <span>This field is required</span>}
+                                        <br />
+                                        <input placeholder="Enter Your Phone Number" style={{ width: "50%" }} type="number" {...register("phone", { required: true })} />
+                                        <br />
+                                        <br />
+                                        <input placeholder="Enter Your Address" style={{ width: "50%" }} type="text" {...register("phone", { required: true })} />
+                                        <br />
+                                        <br />
+                                        <button  style={{ width: "50%" }} className="btn-info">
+                                            <input className="btn-info" type="submit" />
+
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+            </div >
         </div>
     );
 };
--- src/componets/OurServices/OurService.js
+++ src/componets/OurServices/OurService.js
@@ -1,9 +1,49 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Col, Row, Container, Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Order from './../Order/Order';
 const OurService = () => {
+    const [products, setProducts] = useState([])
+
+
+    // Loading data form mongodb
+
+    useEffect(() => {
+        const url = `http://localhost:5000/products`;
+        fetch(url)
+            .then(res => res.json())
+            .then(resut => setProducts(resut))
+
+
+    }, [])
     return (
         <div>
-            <h3>THis is Home</h3>
+            <div><h1 className="text-muted text-center"> Our <span className="text-info"> Collection</span><br />
+            </h1></div>
+            <div className="row">
+                {
+                    products.map(order =>
+                        <div className="col-lg-6 col-sm-12 my-2 p-2">
+                            <div className="card mb-3" style={{ "max - width": "540px" }}>
+                                <div className="row g-0">
+                                    <div className="col-md-4">
+                                        <img src={order.img} className="img-fluid rounded-start mt-3  ms-2" alt="..." />
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 className="card-title">{order.name}</h5>
+                                            <h6 className="card-title my-1">{order.subname}</h6>
+                                            <p className="card-text my-1">{order.discription}</p>
+                                            <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                            <p className="card-text"><small class="text-muted"> {order.price}$</small></p>
+                                            <button className="btn btn-info">Bye Now</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>)
+                }
+            </div>
         </div>
     );
 };
